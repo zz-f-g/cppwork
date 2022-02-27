2ce0e6a372b58aa06df17c8fef31b438bbb7e9a2
zz-f-g <zzguo2052110@outlook.com>
2022-02-27 22:29:14 +0800

    launch the project of 8 queens

--- homework/8queens/8queens.cpp
+++ homework/8queens/8queens.cpp
@@ -2,25 +2,25 @@
 
 int isattacking(int pos[], int num_queen, int newpos)
 {
-    int x = 0;
-    int y = 0;
-    int flag = 0;
-    for (x = 0; x < num_queen; x++)
-    {
-        if ((newpos - pos[x]==0) || (newpos - pos[x]==x-num_queen) || (newpos - pos[x]==num_queen-x))
-        {
-            flag = 1;
-            break;
-        }
-    }
-    return flag;
+	int x = 0;
+	int y = 0;
+	int flag = 0;
+	for (x = 0; x < num_queen; x++)
+	{
+		if ((newpos - pos[x] == 0) || (newpos - pos[x] == x - num_queen) || (newpos - pos[x] == num_queen - x))
+		{
+			flag = 1;
+			break;
+		}
+	}
+	return flag;
 }
 
 int main()
 {
-    int pos[8] = { 0 };
-    pos[1] = 2;
-    int n = 2;
-    std::cout << isattacking(pos,n,5) << std::endl;
-    return 0;
+	int pos[8] = { 0 };
+	pos[1] = 2;
+	int n = 2;
+	std::cout << isattacking(pos, n, 5) << std::endl;
+	return 0;
 }
